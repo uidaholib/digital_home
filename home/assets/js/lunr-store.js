972ddd6f4033932e3cb1f8120c54568e479d9678
evanwill <evanpeterw@gmail.com>
2024-10-06 22:52:58 -0700

    apply correct filter to metadata

--- home/assets/js/lunr-store.js
+++ home/assets/js/lunr-store.js
@@ -1,11 +1,7 @@
 ---
 # create lunr store for search page
 ---
-{% if site.data.theme.search-child-objects == false %}
-{%- assign items = site.data[site.metadata] | where_exp: 'item','item.objectid and item.parentid == nil' -%}
-{% else %}
-{%- assign items = site.data[site.metadata] | where_exp: 'item','item.objectid' -%}
-{% endif %}
+{%- assign items = site.data[site.metadata] | where_exp: 'item','item.objectid and item.status == "published"' -%}
 {%- assign fields = site.data.config-search -%}
 var store = [ 
 {%- for item in items -%} 
